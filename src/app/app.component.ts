d617fd1fb32ac17e89cc5f7c0278c468c66ac30c
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-08-25 04:05:43 +0200

    feat: adding buttons and functions to dispatch actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { appState } from './app.reducer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Application with Angular and NgRx';
+  dato$: Observable<any>;
+
+  constructor(private store: Store<appState>){
+    this.dato$ = store.select('texto');
+  }
+
+  spanishMsj(){
+    this.store.dispatch({type: "SPANISH"});
+  }
+
+  englishMsj(){
+    this.store.dispatch({type: "ENGLISH"});
+  }
 }
